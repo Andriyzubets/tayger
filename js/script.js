3dde2fcd0d5db3a62bd971d28700ba22ad9bca5e
Zubetsandriy <zubets.andriy@gmail.com>
2018-05-15 11:52:23 +0300

    test2

--- js/script.js
+++ js/script.js
@@ -328,7 +328,7 @@ if(document.querySelector('.menu')) {
 		menuButts[5].classList.add('active');
 		document.querySelector('.menu .nav ul').classList.remove('op');
 		document.querySelector('.menu .nav .sandw').classList.remove('active')
-		
+
 		setTimeout(function(){
 			menuTabs[0].classList.remove('active');
 			menuTabs[1].classList.remove('active');
@@ -351,7 +351,7 @@ function init() {
 		zoom: 15,
 		scrollwheel: false,
 		disableDefaultUI: true,
-		center: new google.maps.LatLng(50.4432151, 30.5201291),
+		center: new google.maps.LatLng(50.4432151, 30.5281291),
 		styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
 	};
 	var mapElement = document.getElementById('map');
